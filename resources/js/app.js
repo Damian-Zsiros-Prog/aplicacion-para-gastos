7aa2278693493c78a27404064ca949a0637ded6b
Damian Zsiros Dev <damjeronzalez23@gmail.com>
2021-11-16 14:25:26 -0500

    add style at form, table and responsive style

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,30 @@
+$(document).ready(function () {
+    $("select").formSelect()
+})
 const form = document.getElementById("transactionForm")
 
 window.addEventListener("DOMContentLoaded", function () {
     let caregories = [
         {
             name: "Alquiler",
+            imgSrc: "https://x6i2p6h3.rocketcdn.me/wp-content/uploads/2017/06/negocios-de-alquiler-1.jpg",
         },
         {
             name: "Comida",
+            imgSrc: "https://www.pequerecetas.com/wp-content/uploads/2021/03/comidas-rapidas.jpg",
         },
         {
             name: "Diversion",
+            imgSrc: "https://files.mormonsud.org/wp-content/uploads/2019/06/personas-2.jpg",
+        },
+        {
+            name: "Trabajo",
+            imgSrc: "https://www.abc.com.py/resizer/zI8YapDOpV7tgYfZZgxLDt3rfJ8=/fit-in/770x495/smart/arc-anglerfish-arc2-prod-abccolor.s3.amazonaws.com/public/SGMHXX55ONHBVKDC3RELVMZZCE.jpg",
         },
     ]
     addCategoriesAtSelect(caregories)
     let transactionsHistoryObject =
         JSON.parse(localStorage.getItem("transactionsHistory")) || []
-    console.log(typeof transactionsHistoryObject)
     listTransactionsInTale(transactionsHistoryObject)
 })
 
@@ -38,8 +47,8 @@ form.addEventListener("submit", function (e) {
 function addCategoriesAtSelect(categories) {
     let selectCategory = document.getElementById("categoryTransaction")
     categories.map((category) => {
-        const { name } = category
-        const categoryHTMLAtInsert = `<option value="${name}">${name}</option>`
+        const { name, imgSrc } = category
+        const categoryHTMLAtInsert = `<option data-icon="${imgSrc}" class="left" value="${name}">${name}</option>`
         selectCategory.insertAdjacentHTML("beforeend", categoryHTMLAtInsert)
     })
 }
@@ -55,8 +64,8 @@ function listTransactionsInTale(transactionsHistoryObject) {
 
 function getTransactionId(reverse = false) {
     let lastTransactionId = localStorage.getItem("lastTransactionId") || "-1"
-    let newTransactionId = !reverse
-        ? parseInt(lastTransactionId) + 1
+    let newTransactionId = reverse
+        ? parseInt(lastTransactionId) - 1
         : parseInt(lastTransactionId) + 1
     localStorage.setItem("lastTransactionId", newTransactionId)
     return newTransactionId
@@ -88,18 +97,29 @@ function insertNewTransactionAtTable(transactionDataObject) {
     let descripcionCellRef = newTransactionRowRef.insertCell(1)
     descripcionCellRef.textContent = transactionDataObject.descripcion
     let montoCellRef = newTransactionRowRef.insertCell(2)
-    montoCellRef.textContent = transactionDataObject.monto
+    montoCellRef.textContent = `${transactionDataObject.monto} COP`
     let categoryCellRef = newTransactionRowRef.insertCell(3)
     categoryCellRef.textContent = transactionDataObject.categoria
     let actionsCellRef = newTransactionRowRef.insertCell(4)
 
     let deleteButton = document.createElement("button")
-    deleteButton.textContent = "Eliminar"
+    deleteButton.classList.add("btn-floating")
+    deleteButton.classList.add("btn-large")
+    deleteButton.classList.add("waves-effect")
+    deleteButton.classList.add("waves-light")
+    deleteButton.classList.add("red")
+    deleteButton.title = "Eliminar transaccion"
+
+    let iconDelete = document.createElement("i")
+    deleteButton.classList.add("material-icons")
+    iconDelete.textContent = "close"
+
+    deleteButton.appendChild(iconDelete)
     actionsCellRef.appendChild(deleteButton)
 
     deleteButton.addEventListener("click", function (e) {
         try {
-            let transactionAtDelete = e.target.parentNode.parentNode
+            let transactionAtDelete = e.target.parentNode.parentNode.parentNode
             deleteTransaction(transactionAtDelete.getAttribute("transactionId"))
             transactionAtDelete.remove()
         } catch (error) {
@@ -127,14 +147,13 @@ function deleteTransaction(transactionId) {
         JSON.parse(localStorage.getItem("transactionsHistory"))
     )
 
-    console.log(transactionsHistory)
-
     let transactionIndexInArray = Number(
         transactionsHistory.findIndex(
             (element) => (element.transactionId !== transactionId) - 1
         )
     )
     transactionsHistory.splice(transactionIndexInArray)
+    getTransactionId(true)
 
     let transactionsHistoryJSON = JSON.stringify(transactionsHistory)
     localStorage.setItem("transactionsHistory", transactionsHistoryJSON)
