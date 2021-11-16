1fb196c55b1df4789a25f289f2fc5b8685f4a397
Damian Zsiros Dev <damjeronzalez23@gmail.com>
2021-11-16 12:50:59 -0500

    add functions for selectors and refactorizate

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,129 +1,141 @@
- const form = document.getElementById("transactionForm")
+const form = document.getElementById("transactionForm")
 
-            window.addEventListener("DOMContentLoaded", function () {
-                let transactionsHistoryObject =
-                    JSON.parse(localStorage.getItem("transactionsHistory")) ||
-                    []
-                console.log(transactionsHistoryObject)
-                listTransactionsInTale(transactionsHistoryObject)
-            })
+window.addEventListener("DOMContentLoaded", function () {
+    let caregories = [
+        {
+            name: "Alquiler",
+        },
+        {
+            name: "Comida",
+        },
+        {
+            name: "Diversion",
+        },
+    ]
+    addCategoriesAtSelect(caregories)
+    let transactionsHistoryObject =
+        JSON.parse(localStorage.getItem("transactionsHistory")) || []
+    console.log(typeof transactionsHistoryObject)
+    listTransactionsInTale(transactionsHistoryObject)
+})
 
-            /**
-             * @param {Event} e
-             */
-            form.addEventListener("submit", function (e) {
-                e.preventDefault()
-                let transactionFormData = new FormData(form)
-                let transactionDataObject =
-                    convertFormDataToTransactionObject(transactionFormData)
-                insertNewTransactionAtTable(transactionDataObject)
-                saveTransactionDataObject(transactionDataObject)
-                form.reset()
-            })
+/**
+ * @param {Event} e
+ */
+form.addEventListener("submit", function (e) {
+    e.preventDefault()
+    let transactionFormData = new FormData(form)
+    let transactionDataObject =
+        convertFormDataToTransactionObject(transactionFormData)
+    insertNewTransactionAtTable(transactionDataObject)
+    saveTransactionDataObject(transactionDataObject)
+    form.reset()
+})
 
-            /**
-             * @param {Number} transactionId
-             */
-            function deleteTransaction(transactionId) {
-                let transactionsHistory = Array(
-                    JSON.parse(localStorage.getItem("transactionsHistory"))
-                )
+/**
+ * @param {Array} categories
+ */
+function addCategoriesAtSelect(categories) {
+    let selectCategory = document.getElementById("categoryTransaction")
+    categories.map((category) => {
+        const { name } = category
+        const categoryHTMLAtInsert = `<option value="${name}">${name}</option>`
+        selectCategory.insertAdjacentHTML("beforeend", categoryHTMLAtInsert)
+    })
+}
 
-                let transactionIndexInArray = Number(
-                    transactionsHistory.findIndex(
-                        (element) => element.transactionId !== transactionId
-                    )
-                )
+/**
+ * @param {Array} transactionsHistoryObject
+ */
+function listTransactionsInTale(transactionsHistoryObject) {
+    return transactionsHistoryObject.forEach((transaction) =>
+        insertNewTransactionAtTable(transaction)
+    )
+}
 
-                transactionsHistory.splice(transactionIndexInArray)
+function getTransactionId(reverse = false) {
+    let lastTransactionId = localStorage.getItem("lastTransactionId") || "-1"
+    let newTransactionId = !reverse
+        ? parseInt(lastTransactionId) + 1
+        : parseInt(lastTransactionId) + 1
+    localStorage.setItem("lastTransactionId", newTransactionId)
+    return newTransactionId
+}
 
-                let transactionsHistoryJSON =
-                    JSON.stringify(transactionsHistory)
-                localStorage.setItem(
-                    "transactionsHistory",
-                    transactionsHistoryJSON
-                )
-            }
+/**
+ * @param {FormData} transactionFormData
+ */
+function convertFormDataToTransactionObject(transactionFormData) {
+    return {
+        tipo: transactionFormData.get("typeSelector"),
+        descripcion: transactionFormData.get("descripcionTransaction"),
+        monto: transactionFormData.get("montoTransaction"),
+        categoria: transactionFormData.get("categoryTransaction"),
+        id: getTransactionId(),
+    }
+}
 
-            /**
-             * @param {Array} transactionsHistoryObject
-             */
-            function listTransactionsInTale(transactionsHistoryObject) {
-                return transactionsHistoryObject.forEach((transaction) =>
-                    insertNewTransactionAtTable(transaction)
-                )
-            }
+/**
+ * @param {Object} transactionDataObject
+ */
+function insertNewTransactionAtTable(transactionDataObject) {
+    let transactionTableRef = document.getElementById("transactionTable")
+    let newTransactionRowRef = transactionTableRef.insertRow(-1)
+    newTransactionRowRef.setAttribute("transactionId", transactionDataObject.id)
 
-            function getTransactionId() {
-                let lastTransactionId =
-                    localStorage.getItem("lastTransactionId") || "-1"
-                let newTransactionId = parseInt(lastTransactionId) + 1
-                localStorage.setItem("lastTransactionId", newTransactionId)
-                return newTransactionId
-            }
+    let typeCellRef = newTransactionRowRef.insertCell(0)
+    typeCellRef.textContent = transactionDataObject.tipo
+    let descripcionCellRef = newTransactionRowRef.insertCell(1)
+    descripcionCellRef.textContent = transactionDataObject.descripcion
+    let montoCellRef = newTransactionRowRef.insertCell(2)
+    montoCellRef.textContent = transactionDataObject.monto
+    let categoryCellRef = newTransactionRowRef.insertCell(3)
+    categoryCellRef.textContent = transactionDataObject.categoria
+    let actionsCellRef = newTransactionRowRef.insertCell(4)
 
-            /**
-             * @param {FormData} transactionFormData
-             */
-            function convertFormDataToTransactionObject(transactionFormData) {
-                return {
-                    tipo: transactionFormData.get("typeSelector"),
-                    descripcion: transactionFormData.get(
-                        "descripcionTransaction"
-                    ),
-                    monto: transactionFormData.get("montoTransaction"),
-                    categoria: transactionFormData.get("categoryTransaction"),
-                    id: getTransactionId(),
-                }
-            }
+    let deleteButton = document.createElement("button")
+    deleteButton.textContent = "Eliminar"
+    actionsCellRef.appendChild(deleteButton)
 
-            /**
-             * @param {Object} transactionDataObject
-             */
-            function insertNewTransactionAtTable(transactionDataObject) {
-                let transactionTableRef =
-                    document.getElementById("transactionTable")
-                let newTransactionRowRef = transactionTableRef.insertRow(-1)
-                newTransactionRowRef.setAttribute(
-                    "transactionId",
-                    transactionDataObject.id
-                )
+    deleteButton.addEventListener("click", function (e) {
+        try {
+            let transactionAtDelete = e.target.parentNode.parentNode
+            deleteTransaction(transactionAtDelete.getAttribute("transactionId"))
+            transactionAtDelete.remove()
+        } catch (error) {
+            console.error(error)
+        }
+    })
+}
 
-                let typeCellRef = newTransactionRowRef.insertCell(0)
-                typeCellRef.textContent = transactionDataObject.tipo
-                let descripcionCellRef = newTransactionRowRef.insertCell(1)
-                descripcionCellRef.textContent =
-                    transactionDataObject.descripcion
-                let montoCellRef = newTransactionRowRef.insertCell(2)
-                montoCellRef.textContent = transactionDataObject.monto
-                let categoryCellRef = newTransactionRowRef.insertCell(3)
-                categoryCellRef.textContent = transactionDataObject.categoria
-                let actionsCellRef = newTransactionRowRef.insertCell(4)
+/**
+ * @param {Object} transactionDataObject
+ */
+function saveTransactionDataObject(transactionDataObject) {
+    let transactionsHistory =
+        JSON.parse(localStorage.getItem("transactionsHistory")) || []
+    transactionsHistory.push(transactionDataObject)
+    let transactionDataNew = JSON.stringify(transactionsHistory)
+    return localStorage.setItem("transactionsHistory", transactionDataNew)
+}
 
-                let deleteButton = document.createElement("button")
-                deleteButton.textContent = "Eliminar"
-                actionsCellRef.appendChild(deleteButton)
+/**
+ * @param {Number} transactionId
+ */
+function deleteTransaction(transactionId) {
+    let transactionsHistory = Array.from(
+        JSON.parse(localStorage.getItem("transactionsHistory"))
+    )
 
-                deleteButton.addEventListener("click", function (e) {
-                    let transactionAtDelete = e.target.parentNode.parentNode
-                    transactionAtDelete.remove()
-                    deleteTransaction(
-                        transactionAtDelete.getAttribute("transactionId")
-                    )
-                })
-            }
+    console.log(transactionsHistory)
 
-            /**
-             * @param {Object} transactionDataObject
-             */
-            function saveTransactionDataObject(transactionDataObject) {
-                let transactionsHistory =
-                    JSON.parse(localStorage.getItem("transactionsHistory")) ||
-                    []
-                transactionsHistory.push(transactionDataObject)
-                let transactionDataNew = JSON.stringify(transactionsHistory)
-                return localStorage.setItem(
-                    "transactionsHistory",
-                    transactionDataNew
-                )
-            }
+    let transactionIndexInArray = Number(
+        transactionsHistory.findIndex(
+            (element) => (element.transactionId !== transactionId) - 1
+        )
+    )
+    transactionsHistory.splice(transactionIndexInArray)
+
+    let transactionsHistoryJSON = JSON.stringify(transactionsHistory)
+    localStorage.setItem("transactionsHistory", transactionsHistoryJSON)
+}
